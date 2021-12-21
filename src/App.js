957349798298595f4ddb99dc2e9bbfae608a19d5
MarioAlberto-98 <77813457+MarioAlberto-98@users.noreply.github.com>
2021-12-21 01:36:09 -0600

    Assigment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import AjaxConsult from './Components/AjaxConsult';
+
+function App() {
+  return (
+    <div className="App">
+      <AjaxConsult />
+    </div>
+  );
+}
+
+export default App;
